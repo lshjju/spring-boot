85b5b10c0658f3403d27e60950294d2a6a9fefd9
lshjju <53454978+lshjju@users.noreply.github.com>
2024-07-04 16:08:04 +0900

    Create SecurityConfig.java

---
+++ src/main/java/com.apple.shop/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.apple.shop;
+
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.csrf((csrf) -> csrf.disable());
+    http.authorizeHttpRequests((authorize) ->
+         authorize.requestMatchers("/**").permitAll()
+    );
+    return http.build();
+  }
+}
