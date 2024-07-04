76ac4d7f6d30d7952e9426f7b520367d83180ce3
lshjju <53454978+lshjju@users.noreply.github.com>
2024-07-04 16:59:17 +0900

    Update MemberController.java

--- src/main/java/com.apple.shop/member/MemberController.java
+++ src/main/java/com.apple.shop/member/MemberController.java
@@ -5,8 +5,17 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
+  
   @GetMapping("/register")
-  public String register() {
+  String register() {
     return "register.html";
   }
+
+@PostMapping("/member")
+String addMember() {
+  유저가보낸 아이디비번이름 디비에 저장~~
+  return "redirect:/list";
+} 
+
+  
 } 
