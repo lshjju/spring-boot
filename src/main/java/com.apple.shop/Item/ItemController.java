356a185594134d420d821a0a9e980404afad7412
lshjju <53454978+lshjju@users.noreply.github.com>
2024-07-04 15:57:26 +0900

    Update ItemController.java

--- src/main/java/com.apple.shop/Item/ItemController.java
+++ src/main/java/com.apple.shop/Item/ItemController.java
@@ -62,11 +62,13 @@ String editItem(String title, Integer price, Long id) {
 // itemRepository.save(item)
 
 
-@PostMapping("/test1")
-String test1(@RequestBody Map<String, Object> body) {
-  System.out.println("yochung");
-  return "redirect:/list";
-}
+@DeleteMapping("/item")
+ResponseEntity<String> deleteItem(@RequestParam Long id) {
+  itemRepository.deleteById(id);
+  return ResponseEntity.status(200).body("삭제완료");
+} 
+
+
 
 
 @GetMapping("/detail/{id}")
