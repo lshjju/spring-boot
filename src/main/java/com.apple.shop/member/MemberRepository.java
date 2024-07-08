23786441616d775b289441b784b1eeca2c25f0d8
lshjju <53454978+lshjju@users.noreply.github.com>
2024-07-08 16:36:01 +0900

    Update MemberRepository.java

--- src/main/java/com.apple.shop/member/MemberRepository.java
+++ src/main/java/com.apple.shop/member/MemberRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaReository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+  Optional<Member> findByUsername(String username);
+  
 }
