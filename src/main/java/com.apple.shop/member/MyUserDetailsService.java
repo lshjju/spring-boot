bfee3b869602c84b7b82692b78a6f8676946d5b6
lshjju <53454978+lshjju@users.noreply.github.com>
2024-07-09 21:39:11 +0900

    Update MyUserDetailsService.java

--- src/main/java/com.apple.shop/member/MyUserDetailsService.java
+++ src/main/java/com.apple.shop/member/MyUserDetailsService.java
@@ -10,10 +10,17 @@ public class MyUserDetailsService implements UserDetailsService {
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    // DB에서 username을 가진 유저를 찾아와서
-    // return new User(유저아이디, 비번, 권한) 해주세요
 
-    memberRepository.findByUsername(username);
-  }
+    var result = memberRepository.findByUsername(username);
+    if (result.isEmpty()){
+      throw new UsernameNotFoundException("그런 아이디 없음");    
+    }
+    var user = result.get();
+    List<GrantedAuthority> authorities = new ArrayList<>;
+    authorities.add(new SimpleGrantedAutority("일반유저"));
 
-} 
+    return new User(user.getUsername(), user.getPassword(), autorities);
+
+  } 
+
+}
