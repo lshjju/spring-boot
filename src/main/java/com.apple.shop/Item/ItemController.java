ab5c20a0865c95c8051a3c21d3ead37f2c3bc2b1
lshjju <53454978+lshjju@users.noreply.github.com>
2024-07-10 14:46:43 +0900

    Update ItemController.java

--- src/main/java/com.apple.shop/Item/ItemController.java
+++ src/main/java/com.apple.shop/Item/ItemController.java
@@ -14,88 +14,76 @@ public class ItemController {
     this.itemRepository = itemRepository;
   }
 
-  @PostMapping("/add")
-  String writePost(String title, Integer price) {
-    itemService.saveItem(title, price);
-    return "redirect:/list";
-  }
 
-/*  
-@PostMapping("/add")
-String writePost(String title, Integer price) {
-  Item item = new Item;
-  item.setTitle(title);
-  item.setPrice(price);
-  itemRepository.save(item);
-  return "redirect:/list";
-}  
-*/
+// @PostMapping("/add")
+// String writePost(String title, Integer price) {
+//   itemService.saveItem(title, price);
+//   return "redirect:/list";
+// }
+
 
   
-@GetMapping("/edit/{id}")
-String edit(Model model, @PathVariable Long id) {
-  Optional<Item> result = itemRepository.findById(id);
-  if (result.isPresent()) {
-    model.addAttribute("data", result.get());
-    return "edit.html";
-  } else {
-    return "redirect:/list";
-  }
-}
+ 
+// @PostMapping("/add")
+// String writePost(String title, Integer price) {
+//   Item item = new Item;
+//   item.setTitle(title);
+//   item.setPrice(price);
+//   itemRepository.save(item);
+//   return "redirect:/list";
+// }  
 
-@PostMapping("/edit")
-String editItem(String title, Integer price, Long id) {
+
   
-  Item item = new Item();
-  item.setId(id);
-  item.setTitle(title);
-  item.setPrice(price);
-  itemRepository.save(item);
+// @GetMapping("/edit/{id}")
+// String edit(Model model, @PathVariable Long id) {
+//   Optional<Item> result = itemRepository.findById(id);
+//   if (result.isPresent()) {
+//     model.addAttribute("data", result.get());
+//     return "edit.html";
+//   } else {
+//     return "redirect:/list";
+//   }
+// }
+
   
-  return "redirect:/list";
-}
 
-/*
-var item = new Item();
-item.id = 1L;
-itemRepository.save(item)
-*/
-
-@DeleteMapping("/item/{abc}")
-ResponseEntity<String> deleteItem(@RequestParam Long id) {
-  itemRepository.deleteById(id);
-  return ResponseEntity.status(200).body("삭제완료");
-} 
+// @PostMapping("/edit")
+// String editItem(String title, Integer price, Long id) {
+//   Item item = new Item();
+//   item.setId(id);
+//   item.setTitle(title);
+//   item.setPrice(price);
+//   itemRepository.save(item);
+//   return "redirect:/list";
+// }
 
 
-@GeteMapping("/test2")
-String deleteItem() {
-  var result = new BCryptPasswordEncorder().encode("문자");
-  System.out.println(result);
-  return "redirect:/list";
-}   
+// var item = new Item();
+// item.id = 1L;
+// itemRepository.save(item)
 
 
-@GetMapping("/detail/{id}")
-ResponseEntity<String> detail() {
-    try {
-      throw new Exception("이런저런에러");
-    } catch(Exception e){
-      return ResponseEntity.status(에러코드).body("에러이유");
-    }
-}
 
-@GetMapping("/detail/{id}")
-String detail(@PathVariable Long id, Model model) {
-  
-  Optional<Item> result = itemRepository.findById(id);
-  if (result.isPresent()){
-    model.addAttribute("data", result.get());
-    return "detail.html";
-    } else {
-      return "redirect:/list";
-    }
-  }   
+
+
+// @GeteMapping("/test2")
+// String deleteItem() {
+//   var result = new BCryptPasswordEncorder().encode("문자");
+//   System.out.println(result);
+//   return "redirect:/list";
+// }   
+
+
+// @GetMapping("/detail/{id}")
+// ResponseEntity<String> detail() {
+//     try {
+//       throw new Exception("이런저런에러");
+//     } catch(Exception e){
+//       return ResponseEntity.status(에러코드).body("에러이유");
+//     }
+// }
+
 
 @GetMapping("/write")
 String write() {
@@ -106,11 +94,45 @@ String write() {
 String list(Model model){
   List<Item> result = itemRepository.findAll();
   model.addAttribute("items", result);
-
   return "list.html"
 }
 
 
+@PostMapping("/add")
+String addPost(String title, Integer price) {
+  itemService.saveItem(title, price);
+  return "redirect:/list";
+}
+
+
+  
+@GetMapping("/detail/{id}")
+String detail(@PathVariable Long id, Model model) {
+  
+  Optional<Item> result = itemRepository.findById(id);
+  if (result.isPresent()){
+    model.addAttribute("data", result.get());
+    return "detail.html";
+    } else {
+      return "redirect:/list";
+    }
+  }  
+
+  
+@DeleteMapping("/item/{abc}")
+ResponseEntity<String> deleteItem(@RequestParam Long id) {
+  itemRepository.deleteById(id);
+  return ResponseEntity.status(200).body("삭제완료");
+} 
+
+  
+@GetMapping("/list/page/1")
+String getListPage(Model model){
+  List<Item> result = itemRepository.findAll();
+  model.addAttribute("items", result);
+  return "list.html"
+}  
+  
   
 }
 
