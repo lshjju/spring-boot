a3e88c7b23bde77c987763c3ff1e93d7e9547184
lshjju <53454978+lshjju@users.noreply.github.com>
2024-07-10 14:49:51 +0900

    Update ItemRepository.java

--- src/main/java/com.apple.shop/Item/ItemRepository.java
+++ src/main/java/com.apple.shop/Item/ItemRepository.java
@@ -1,6 +1,10 @@
-package com.apple.shop;
+package com.apple.shop.item;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageble;  
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
+  Page<Item> findPageBy(Pageble page);
 } 
