ea0159f9213059cba0d7277cd45616a8487d64ea
lshjju <53454978+lshjju@users.noreply.github.com>
2024-07-11 22:13:24 +0900

    Update ItemRepository.java

--- src/main/java/com.apple.shop/Item/ItemRepository.java
+++ src/main/java/com.apple.shop/Item/ItemRepository.java
@@ -9,7 +9,7 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
   Page<Item> findPageBy(Pageble page);
   List<Item> findAllByTitleContains(String title);
 
-  @Query(value = "select * from item where id = 2", nativeQuery = true)
-  Item rawQuery1();
+  @Query(value = "select * from item where match(title) against(?1)", nativeQuery = true)
+  List<Item> rawQuery1(String text);
 
 } 
