551b3d71a089eca8da54cbf07331a7ff14e42af1
lshjju <53454978+lshjju@users.noreply.github.com>
2024-07-11 15:38:09 +0900

    Update MemberRepository.java

--- src/main/java/com.apple.shop/member/MemberRepository.java
+++ src/main/java/com.apple.shop/member/MemberRepository.java
@@ -1,6 +1,9 @@
 package com.apple.shop.member;
 
+import com.apple.shop.member.Member;
 import org.springframework.data.jpa.repository.JpaReository;
+
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
