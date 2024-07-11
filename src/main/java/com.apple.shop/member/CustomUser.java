1e7b7e02a6c03b74c3eea4bbdb07f062e666e755
lshjju <53454978+lshjju@users.noreply.github.com>
2024-07-11 15:49:56 +0900

    Create CustomUser.java

---
+++ src/main/java/com.apple.shop/member/CustomUser.java
@@ -0,0 +1,11 @@
+class CustomUser extends User {
+  public String displayName;
+  public CustomUser(
+    String username,
+    String password,
+    Collection<? extends GrantedAuthority> authorities
+  ) {
+    super(username, password, authorities);
+  }
+  
+}
