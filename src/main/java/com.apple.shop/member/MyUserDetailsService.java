b4eed47a31388e8ba438059231842f7b2211430b
lshjju <53454978+lshjju@users.noreply.github.com>
2024-07-11 15:48:59 +0900

    Update MyUserDetailsService.java

--- src/main/java/com.apple.shop/member/MyUserDetailsService.java
+++ src/main/java/com.apple.shop/member/MyUserDetailsService.java
@@ -26,14 +26,4 @@ public class MyUserDetailsService implements UserDetailsService {
 
 }
 
-class Customer extends User {
-  public String displayName;
-  public CustomUser(
-    String username,
-    String password,
-    Collection<? extends GrantedAuthority> authorities
-  ) {
-    super(username, password, authorities);
-  }
-  
-}
+
