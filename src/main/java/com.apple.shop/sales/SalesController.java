a3d973fa9355fd0bc2976d4716ba66d149303deb
lshjju <53454978+lshjju@users.noreply.github.com>
2024-07-11 23:33:37 +0900

    Update SalesController.java

--- src/main/java/com.apple.shop/sales/SalesController.java
+++ src/main/java/com.apple.shop/sales/SalesController.java
@@ -5,15 +5,15 @@ public class SalesController {
   private final SalesRepository salesRepository;
   
   @PostMapping("/order")
-  String postOrder(String title, 
-                   Integer price, 
-                   Integer count, 
+  String postOrder(@RequestParam String title, 
+                   @RequestParam Integer price, 
+                   @RequestParam Integer count, 
                    Authentication auth) {
-  
     Sales sales = new Sales();
-    sales.setTitle(title);
-    sales.setPrice(price);
     sales.setCount(count);
+    sales.setPrice(price);
+    sales.setItemName(title);
+    CustomUser user = (Customuser) auth.getPrincipal();
     sales.setMemberId(???);
     salesRepository.save(sales);
     return "list.html";
