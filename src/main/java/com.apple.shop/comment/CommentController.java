d46ae11d7febfe04845aaa7298c7f80e2e65388b
lshjju <53454978+lshjju@users.noreply.github.com>
2024-07-11 16:09:17 +0900

    Update CommentController.java

--- src/main/java/com.apple.shop/comment/CommentController.java
+++ src/main/java/com.apple.shop/comment/CommentController.java
@@ -8,13 +8,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RequiredArgsConstructor
 public class CommentController {
 
-  private final CommentRepository commentRepository;
+private final CommentRepository commentRepository;
 
-  @PostMapping("/comment")
-String postComment(@RequestParam String content, Authentication auth) {
-  (CustomUser) auth.getPrincipal();
+@PostMapping("/comment")
+String postComment(@RequestParam String content, 
+                   @RequestParam Long parent, 
+                   Authentication auth
+) {
+  CustomUser user = (CustomUser) auth.getPrincipal();
+  
   var data = new Comment();
   data.setContent(content);
+  data.setUsername(user.getUsername());
+  data.setParentId(parent);
   commentRepository.save(data);
   return "redirect:/list";
 }
