a94b3a221bbd5da150b1ae781adb2455b4fc1668
lshjju <53454978+lshjju@users.noreply.github.com>
2024-07-13 23:26:10 +0900

    Update SalesController.java

--- src/main/java/com.apple.shop/sales/SalesController.java
+++ src/main/java/com.apple.shop/sales/SalesController.java
@@ -31,8 +31,7 @@ public class SalesController {
     // System.out.println(result.get(0));
 
     memberRepository.finlById(1L);
-    System.out.println(result);
-    
+    System.out.println(result.get().getSales());    
     return "sales.html";
   }
 
