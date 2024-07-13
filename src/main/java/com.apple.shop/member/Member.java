069831360aeb634a848a36a832d95c391522275a
lshjju <53454978+lshjju@users.noreply.github.com>
2024-07-13 23:16:45 +0900

    Update Member.java

--- src/main/java/com.apple.shop/member/Member.java
+++ src/main/java/com.apple.shop/member/Member.java
@@ -9,12 +9,17 @@ import lombok.Setter;
 @Setter
 @ToString
 public class Member {
+  
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   public Long id;
-
   @Column(unique = true)
   public String username;
   public String displayName;
   public String password;
+
+  @OneToMany(mappedBy = "member")
+  List<Sales> sales = new ArrayList<>();
+
+  
 } 
