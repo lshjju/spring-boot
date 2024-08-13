81c235695ccac759a03c3f5a74907e3eab57ea58
lshjju <53454978+lshjju@users.noreply.github.com>
2024-08-13 22:35:00 +0900

    Update ShopApplication.java

--- src/main/java/com.apple.shop/ShopApplication.java
+++ src/main/java/com.apple.shop/ShopApplication.java
@@ -1,1 +1,13 @@
-a
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopApplication.class, args);
+	}
+
+}
