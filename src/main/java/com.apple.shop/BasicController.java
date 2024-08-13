208514a9ef1ca538edaf74fa267205ed7c17dae0
lshjju <53454978+lshjju@users.noreply.github.com>
2024-08-13 22:53:06 +0900

    Update BasicController.java

--- src/main/java/com.apple.shop/BasicController.java
+++ src/main/java/com.apple.shop/BasicController.java
@@ -1,17 +1,17 @@
 @Controller
 public class BasicController {
     @GetMapping("/")
-    @ResponseBody
     String hello(){
-        return "hello";
+        return "index.html";
+    }   
+
+    @GetMapping("/about")
+    String about(){
+        return "피싱사이트예요";
     }   
 }
 
-
-
-
 ///////////////////////////
-
 package com.apple.shop;
 
 @Controller
