d65a89a26ef382e3043bb057fd900af2e9938785
lshjju <53454978+lshjju@users.noreply.github.com>
2024-08-26 01:08:23 +0900

    Update MyUserDetailsService.java

--- src/main/java/com.apple.shop/member/MyUserDetailsService.java
+++ src/main/java/com.apple.shop/member/MyUserDetailsService.java
@@ -1,3 +1,32 @@
+@RequiredArgsConstructor
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+  private final MemberRepository memberRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+    var result = memberRepository.findByUsername(username);
+    if (result.isEmpty()){
+      throw new UsernameNotFoundExeception("그런아이디 없음");
+    }
+    var user = result.get();
+
+    List<GrantedAutority> autorities = new ArrayList<>();
+    authorities.add(new SimpleGrantedAuthority("일반유저"));
+
+    return new User(user.getUsername(), user.getPassword(), authorities)
+
+
+
+
+    
+
+  } 
+
+}
+/////////////////////////////////
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
