d5c2ba431aa9dd99ac9b7acfccc0b5062f8541ae
lshjju <53454978+lshjju@users.noreply.github.com>
2024-08-29 00:08:43 +0900

    Update MyUserDetailsService.java

--- src/main/java/com.apple.shop/member/MyUserDetailsService.java
+++ src/main/java/com.apple.shop/member/MyUserDetailsService.java
@@ -22,46 +22,9 @@ public class MyUserDetailsService implements UserDetailsService {
 
 }
 
-class CustomUser extends User {
-  public String displayName;
-  public CustomUser(
-                   String username,
-                   String password,
-                   List<GrantedAuthority> authorities 
-  ) {
-    super(username, password, authorities);
-  }
-}
+
 
 /////////////////////////////////
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
-@Service
-public class MyUserDetailsService implements UserDetailsService {
-
-  private final MemberRepository memberRepository;
-
-  @Override
-  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-    var result = memberRepository.findByUsername(username);
-    if (result.isEmpty()){
-      throw new UsernameNotFoundException("그런 아이디 없음");    
-    }
-    var user = result.get();
-    List<GrantedAuthority> authorities = new ArrayList<>();
-    authorities.add(new SimpleGrantedAutority("일반유저"));
-    var a = new Customer(user.getUsername(), user.getPassword(), authorities);
-    a.displayName = user.getDisplayName();
-    a.id = user.getId();
-    
-    return a;
-
-  } 
-
-}
 
 
