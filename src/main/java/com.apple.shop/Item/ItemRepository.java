e642c722f34b07077c5af59eb3e28c1a3ebec8cc
lshjju <53454978+lshjju@users.noreply.github.com>
2024-08-29 01:44:06 +0900

    Update ItemRepository.java

--- src/main/java/com.apple.shop/Item/ItemRepository.java
+++ src/main/java/com.apple.shop/Item/ItemRepository.java
@@ -3,6 +3,10 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
  Page<Item> findPageBy(Pageable page);
  List<Item> findAllByTitleConatins(String title);
 
+ @Query(value = "select * from item where id = 2", nativeQuery = true)
+ Item rawQuery1();
+ 
+
 } 
 /////////////////////////////////////
 
