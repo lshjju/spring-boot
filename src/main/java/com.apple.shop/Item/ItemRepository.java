379e5013de2ac9d88f8036e4a71f46bb51624cdd
lshjju <53454978+lshjju@users.noreply.github.com>
2024-08-29 01:07:04 +0900

    Update ItemRepository.java

--- src/main/java/com.apple.shop/Item/ItemRepository.java
+++ src/main/java/com.apple.shop/Item/ItemRepository.java
@@ -1,21 +1,17 @@
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
  Page<Item> findPageBy(Pageable page);
+ List<Item> findAllByTitleConatins(String title);
 
 } 
 /////////////////////////////////////
-package com.apple.shop.item;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageble;  
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
 
-  Page<Item> findPageBy(Pageble page);
-  List<Item> findAllByTitleContains(String title);
+
+
 
   @Query(value = "select * from item where match(title) against(?1)", nativeQuery = true)
   List<Item> rawQuery1(String text);
 
-} 
+
