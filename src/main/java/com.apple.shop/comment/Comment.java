44b5eb00c50d40a9959311f870262c671558844f
lshjju <53454978+lshjju@users.noreply.github.com>
2024-08-28 23:51:05 +0900

    Update Comment.java

--- src/main/java/com.apple.shop/comment/Comment.java
+++ src/main/java/com.apple.shop/comment/Comment.java
@@ -1,19 +1,19 @@
-package com.apple.shop.comment;
-
-import jakarta.persistence.Entity;
-import lombok.Getter;
-import lombok.Setter;
-
 @Entity
 @Getter
 @Setter
 @ToString
-  
 public class Comment {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  public Long id;
-  public String username;
-  public String content;
-  public Long parentId;
+  private Long id;
+  private String username;
+  @Column(length = 1000)
+  private String content;
+  private Long parentId;
 }
+
+
+///////////////////////////////
+
+
+
