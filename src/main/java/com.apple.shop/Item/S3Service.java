5561460e509aef8393e81e511e7279a4e6520bd1
lshjju <53454978+lshjju@users.noreply.github.com>
2024-08-28 18:10:49 +0900

    Update and rename S3Service to S3Service.java

--- src/main/java/com.apple.shop/Item/S3Service.java
+++ src/main/java/com.apple.shop/Item/S3Service.java
@@ -1,3 +1,26 @@
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+  @Value("${spring.cloud.aws.s3.bucket}")
+  private String bucket;
+  private final S3Presigner s3Presigner;
+
+  String createPresignedUrl(String path) {
+    var putObjectRequest = PutObjectRequest.builder()
+           .bucket(bucket)
+           .key(path)
+           .build();
+    var preSignRequest = PutObjectPresignRequest.builder()
+           .signatureDuration(Duration.ofMinutes(3))
+           .putObjectRequest(putObjectRequest)
+           .build();
+    return s3Presigner.presignPutObject(preSignRequest).url().toString();
+ }
+
+}
+
+////////////////////////////
 package com.apple.shop.item;
 
 import org.springframework.stereotype.Service;
