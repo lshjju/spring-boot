65aaec797a37f855ac1d3581e0539b3c1c610fd1
lshjju <53454978+lshjju@users.noreply.github.com>
2024-08-29 00:23:10 +0900

    Update CommentRepository.java

--- src/main/java/com.apple.shop/comment/CommentRepository.java
+++ src/main/java/com.apple.shop/comment/CommentRepository.java
@@ -5,16 +5,4 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
 
 ///////////////////////////
-package com.apple.shop.comment;
 
-import org.springframework.data.jpa.repository.JpaReository;
-
-import java.util.List;
-// import com.apple.shop.member.Member;
-// import java.util.Optional;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-  List<Comment> findAllByParentId(Long parentId);
-  
-}
