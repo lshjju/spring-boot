65f6e0c11c974a60e5e404c3c968d90b65e93dec
lshjju <53454978+lshjju@users.noreply.github.com>
2024-08-29 00:20:01 +0900

    Update CommentController.java

--- src/main/java/com.apple.shop/comment/CommentController.java
+++ src/main/java/com.apple.shop/comment/CommentController.java
@@ -5,45 +5,24 @@ public class CommentController {
   private final CommentRepository commentRepository;
 
   @PostMapping("/comment")
-  String postComment(@RequestParam String content, Authentication auth){
-    (CustomUser) auth.getPrincipal();
+  String postComment(
+    @RequestParam String content, 
+    @RequestParam Long parent, 
+    Authentication auth
+  ){
+    CustomUser user = (CustomUser) auth.getPrincipal();
+    
     var data = new Comment();
-    data.setContent("sdfsdf");
+    data.setContent(content);
+    data.setUsername(user.getUsername());
+    data.setParentId(parent);
     commentRepository.save(data);
-    return "";
+    return "redirect:/list";
   }
 }
 ///////////////////////
-package com.apple.shop.comment;
-
-import lombok.RequredArgsConstrutor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-
-@Controller
-@RequiredArgsConstructor
-public class CommentController {
-
-private final CommentRepository commentRepository;
-
-@PostMapping("/comment")
-String postComment(@RequestParam String content, 
-                   @RequestParam Long parent, 
-                   Authentication auth
-) {
-  CustomUser user = (CustomUser) auth.getPrincipal();
-  
-  var data = new Comment();
-  data.setContent(content);
-  data.setUsername(user.getUsername());
-  data.setParentId(parent);
-  commentRepository.save(data);
-  return "redirect:/list";
-}
 
 
 
-  
-}
 
 
