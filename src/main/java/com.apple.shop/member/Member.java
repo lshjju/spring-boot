597261465c855073f7f5f418bc291088d80897d5
lshjju <53454978+lshjju@users.noreply.github.com>
2024-09-12 00:42:41 +0900

    Update Member.java

--- src/main/java/com.apple.shop/member/Member.java
+++ src/main/java/com.apple.shop/member/Member.java
@@ -1,6 +1,7 @@
 @Entity
 @Getter
 @Setter  
+@ToString
 public class Member {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,29 +14,11 @@ public class Member {
 }
 
 ///////////////////////////
-package com.apple.shop.member;
 
-import jakarta.persistence.Entity;
-import lombok.Getter;
-import lombok.Setter;
-
-@Entity
-@Getter
-@Setter
-@ToString
-public class Member {
-  
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
-  @Column(unique = true)
-  private String username;
-  private String displayName;
-  private String password;
 
   @ToString.Exclude
   @OneToMany(mappedBy = "member")
   private List<Sales> sales = new ArrayList<>();
 
   
-} 
+
