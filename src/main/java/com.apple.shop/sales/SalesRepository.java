16bb5e053f3ab1edf0270b6239b978bab386cad9
lshjju <53454978+lshjju@users.noreply.github.com>
2024-09-12 01:10:40 +0900

    Update SalesRepository.java

--- src/main/java/com.apple.shop/sales/SalesRepository.java
+++ src/main/java/com.apple.shop/sales/SalesRepository.java
@@ -3,16 +3,12 @@ public interface SalesRepository extends JpaRepository<Sales, Long> {
   @Query(value = "SELECT s FROM Sales s JOIN FETCH s.member")
   List<Sales> customFindAll();
   
-
 }
 
-/////////////////////////
 
 
-public interface SalesRepository extends JpaRepository<Sales, Long> {
 
-  @Query(value = "SELECT s FROM Sales s JOIN FETCH s.member")
-  List<Sales> customFindAll();
+ 
   
 
-}
+
