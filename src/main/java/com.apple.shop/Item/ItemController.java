1149dd6504a2170be1e63e9b3bda6a6089ec806d
lshjju <53454978+lshjju@users.noreply.github.com>
2024-09-24 18:13:26 +0900

    Update ItemController.java

--- src/main/java/com.apple.shop/Item/ItemController.java
+++ src/main/java/com.apple.shop/Item/ItemController.java
@@ -1,3 +1,31 @@
+package jinuk.pj1;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping("/list")
+    String list(Model model){
+        List<Item> result = itemRepository.findAll();
+        model.addAttribute("items", result);
+        var a = new Item();
+        System.out.println(a.toString());
+
+        return "list.html";
+    }
+}
+
+
+////////////////////////////////////
 @Controller
 @RequiredArgsConstructor
 public class ItemController {
