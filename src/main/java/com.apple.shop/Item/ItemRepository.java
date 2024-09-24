6cd18bf7651ac8b8bedcdcbd6b28c9e39bc4e710
lshjju <53454978+lshjju@users.noreply.github.com>
2024-09-24 18:12:10 +0900

    Update ItemRepository.java

--- src/main/java/com.apple.shop/Item/ItemRepository.java
+++ src/main/java/com.apple.shop/Item/ItemRepository.java
@@ -1,3 +1,12 @@
+package jinuk.pj1;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
+
+
+/////////////////////////////////
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
  Page<Item> findPageBy(Pageable page);
