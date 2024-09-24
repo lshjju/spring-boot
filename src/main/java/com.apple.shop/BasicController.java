41ea515e74fc9c9c741493fbc9487c4bf2ba49b6
lshjju <53454978+lshjju@users.noreply.github.com>
2024-09-24 18:14:58 +0900

    Update BasicController.java

--- src/main/java/com.apple.shop/BasicController.java
+++ src/main/java/com.apple.shop/BasicController.java
@@ -1,3 +1,24 @@
+package jinuk.pj1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BasicController {
+    @GetMapping("/")
+    String hello() {
+        return "index.html";
+    }
+
+    @GetMapping("/about")
+    @ResponseBody
+    String about() {
+        return "its a fishing";
+    }
+}
+
+/////////////////////////////////
 @Controller
 public class BasicController {
     @GetMapping("/")
