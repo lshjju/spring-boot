e20d1cc9fe0454aed11bfb0fc798705971c9eab2
lshjju <53454978+lshjju@users.noreply.github.com>
2024-09-24 18:15:39 +0900

    Update ShopApplication.java

--- src/main/java/com.apple.shop/ShopApplication.java
+++ src/main/java/com.apple.shop/ShopApplication.java
@@ -1,3 +1,19 @@
+package jinuk.pj1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pj1Application.class, args);
+	}
+
+}
+
+
+/////////////////////////////
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
