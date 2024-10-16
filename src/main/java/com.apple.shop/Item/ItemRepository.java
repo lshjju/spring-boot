96b3a33a67691b990d95b2a1b047ed2117cf1933
lshjju <53454978+lshjju@users.noreply.github.com>
2024-10-16 23:52:57 +0900

    Update ItemRepository.java

--- src/main/java/com.apple.shop/Item/ItemRepository.java
+++ src/main/java/com.apple.shop/Item/ItemRepository.java
@@ -1,3 +1,12 @@
+package jinuk.project3.item;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
+
+
+//////////////////////////////////////
 package jinuk.pj1;
 
 import org.springframework.data.jpa.repository.JpaRepository;
