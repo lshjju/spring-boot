b4bd000f1dd0c2e781b95f823404f3a5dbb5e367
lshjju <53454978+lshjju@users.noreply.github.com>
2024-10-16 23:51:46 +0900

    Update Item.java

--- src/main/java/com.apple.shop/Item/Item.java
+++ src/main/java/com.apple.shop/Item/Item.java
@@ -1,3 +1,28 @@
+package jinuk.project3.item;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@ToString
+@Getter
+@Setter
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    private String title;
+    private Integer price;
+
+}
+
+
+///////////////////////////////
 package jinuk.pj1;
 
 import jakarta.persistence.Entity;
