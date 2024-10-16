3327c74f634f1973a4c5ac311f6ebb5ee7362dca
lshjju <53454978+lshjju@users.noreply.github.com>
2024-10-16 23:52:26 +0900

    Update ItemController.java

--- src/main/java/com.apple.shop/Item/ItemController.java
+++ src/main/java/com.apple.shop/Item/ItemController.java
@@ -1,3 +1,98 @@
+package jinuk.project3.item;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemRepository itemRepository;
+    private final ItemService itemService;
+
+    @GetMapping("/list")
+    String list(Model model){
+        List<Item> result = itemRepository.findAll();
+//        System.out.println(result.get(0).price);
+//        System.out.println(result.get(0).title);
+        model.addAttribute("items", result);
+        return "list.html";
+    }
+
+    @GetMapping("/write")
+    String write(){
+        return "write.html";
+    }
+
+    @PostMapping("/add")
+    String addPost(@RequestParam String title, @RequestParam Integer price){
+//        System.out.println(title);
+//        System.out.println(price);
+        itemService.saveItem(title, price);
+
+        return "redirect:/list";
+
+    }
+
+    @GetMapping("/detail/{id}")
+    String detail(@PathVariable Long id, Model model){
+        Optional<Item> result = itemRepository.findById(id);
+//        System.out.println(result.get());
+        if (result.isPresent()) {
+            model.addAttribute("data", result.get());
+            return "detail.html";
+        } else {
+            return "redirect:/list";
+
+        }
+
+    }
+
+    @GetMapping("/edit/{id}")
+    String edit(Model model, @PathVariable Long id){
+
+        Optional<Item> result = itemRepository.findById(id);
+        if (result.isPresent()) {
+            model.addAttribute("data", result.get());
+            return "edit.html";
+        } else {
+            return "redirect:/list";
+
+        }
+    }
+
+    @PostMapping("/edit")
+    String editItem(String title, Integer price, Long id){
+
+        Item item = new Item();
+        item.setId(id);
+        item.setTitle(title);
+        item.setPrice(price);
+        itemRepository.save(item);
+
+        return "redirect:/list";
+
+    }
+
+    @DeleteMapping("/item")
+    ResponseEntity<String> deleteItem(@RequestParam Long id){
+        itemRepository.deleteById(id);
+        return ResponseEntity.status(200).body("삭제완료");
+    }
+
+
+
+}
+
+
+
+////////////////////////////////////////////
 package jinuk.pj1;
 
 import lombok.RequiredArgsConstructor;
