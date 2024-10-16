ed21f1e8018a6633464488ac28f6ca10f82092ae
lshjju <53454978+lshjju@users.noreply.github.com>
2024-10-17 00:01:28 +0900

    Update and rename ShopApplication.java to ProjectNApplication.java

--- src/main/java/com.apple.shop/ProjectNApplication.java
+++ src/main/java/com.apple.shop/ProjectNApplication.java
@@ -1,3 +1,20 @@
+package jinuk.project3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project3Application.class, args);
+	}
+
+}
+
+
+
+////////////////////////////////
 package jinuk.pj1;
 
 import org.springframework.boot.SpringApplication;
