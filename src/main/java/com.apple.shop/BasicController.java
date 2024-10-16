9cb740a2182aeebfc4103b141a8668939fabd852
lshjju <53454978+lshjju@users.noreply.github.com>
2024-10-16 23:59:33 +0900

    Update BasicController.java

--- src/main/java/com.apple.shop/BasicController.java
+++ src/main/java/com.apple.shop/BasicController.java
@@ -1,3 +1,27 @@
+package jinuk.project3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BasicController {
+
+    @GetMapping("/")
+    String hello(){
+        return "index.html";
+    }
+
+    @GetMapping("/about")
+    @ResponseBody
+    String about(){
+        return "피싱사이트예요";
+    }
+}
+
+
+
+/////////////////////////////
 package jinuk.pj1;
 
 import org.springframework.stereotype.Controller;
