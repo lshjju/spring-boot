e019d0d26fc64d21fab69107c514e9f73f2672a3
lshjju <53454978+lshjju@users.noreply.github.com>
2024-10-18 01:14:41 +0900

    Update MemberController.java

--- src/main/java/com.apple.shop/member/MemberController.java
+++ src/main/java/com.apple.shop/member/MemberController.java
@@ -1,3 +1,40 @@
+package jinuk.project4.member;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @GetMapping("/register")
+    String register(){
+        return "register.html";
+    }
+
+    @PostMapping("/member")
+    String addMember( String username, String password, String displayName) {
+        Member member = new Member();
+        member.setUsername(username);
+        var hash = passwordEncoder.encode(password);
+        member.setPassword(hash);
+        member.setDisplayName(displayName);
+        memberRepository.save(member);
+
+        return "redirect:/list";
+    }
+}
+
+
+
+//////////////////////////////////
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
