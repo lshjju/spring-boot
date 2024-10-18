050b537b3fb9a548ecd4a51585b6fc460f288b90
lshjju <53454978+lshjju@users.noreply.github.com>
2024-10-19 01:23:41 +0900

    Update MemberController.java

--- src/main/java/com.apple.shop/member/MemberController.java
+++ src/main/java/com.apple.shop/member/MemberController.java
@@ -1,3 +1,69 @@
+package jinuk.project5.member;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @GetMapping("/register")
+    String register(){
+        return "register.html";
+    }
+
+    @PostMapping("/member")
+    String addMember(String username, String password, String displayName){
+
+        Member member = new Member();
+        member.setUsername(username);
+//        var hash = new BCryptPasswordEncoder().encode(password);
+        var hash = passwordEncoder.encode(password);
+        member.setPassword(hash);
+        member.setDisplayName(displayName);
+        memberRepository.save(member);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        var result = memberRepository.findByUsername("johndoe");
+        System.out.println(result.get().getUsername());
+        return "login.html";
+    }
+
+    @GetMapping("/my-page")
+    public String mypPage(Authentication auth) {
+//        CustomUser result = (CustomUser) auth.getPrincipal();
+//        System.out.println(result.displayName);
+        System.out.println(auth);
+//        System.out.println(auth.getAuthorities().contains(new SimpleGrantedAuthority("일반유저")));
+//        System.out.println(auth);
+//        System.out.println(auth.getName());
+//        System.out.println(auth.isAuthenticated());
+        return "mypage.html";
+    }
+
+//    @GetMapping("/logout")
+//    public String logout() {
+//        return "logout.html";
+//    }
+
+
+}
+
+
+
+//////////////////////////
 package jinuk.project4.member;
 
 import lombok.RequiredArgsConstructor;
