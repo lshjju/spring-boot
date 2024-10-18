597932bc13db4fc24e3528a200543eca1f97ffeb
lshjju <53454978+lshjju@users.noreply.github.com>
2024-10-19 01:26:59 +0900

    Update Member.java

--- src/main/java/com.apple.shop/member/Member.java
+++ src/main/java/com.apple.shop/member/Member.java
@@ -1,3 +1,24 @@
+package jinuk.project5.member;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    @Column(unique = true)
+    private String username;
+    private String displayName;
+    private String password;
+}
+
+
+//////////////////////////////
 package jinuk.project4.member;
 
 import jakarta.persistence.Entity;
