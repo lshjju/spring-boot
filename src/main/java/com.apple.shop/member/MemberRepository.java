6938f848d40f11b56e1073a9c6b8492041e36f86
lshjju <53454978+lshjju@users.noreply.github.com>
2024-10-19 01:27:37 +0900

    Update MemberRepository.java

--- src/main/java/com.apple.shop/member/MemberRepository.java
+++ src/main/java/com.apple.shop/member/MemberRepository.java
@@ -1,3 +1,16 @@
+package jinuk.project5.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByUsername(String username);
+
+}
+
+/////////////////////////////
 package jinuk.project4.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
